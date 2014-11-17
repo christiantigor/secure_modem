d1db595061d55b83fc54555a8d9de4a1337e5ec3
christiantigor <christian.tigor@rocketmail.com>
2014-11-17 10:16:21 +0700

    update demo compression with matlab

--- Workshop/Demo/audio_compression/codecExample.m
+++ Workshop/Demo/audio_compression/codecExample.m
@@ -1,14 +1,14 @@
 %notes:
 %can not save file to wav, because MATLAB not support INT8 format as needed for
 %linear and mu-law encoding.
+%from the graph we can see that the wave quantization are smaller in linEnc and muEnc signal.
+%in order to compare performance between linear and mu compression, we calculate the avg error of decoded and wave input.
 
 %codec example
 close all;
-f = inline('sign(x)*((log(1+255*abs(x)))/log(256))', 'x');
-%fplot(f,[-1,1]);
 
 %mu-law codec algorithm
-mu = inline('sign(x) * floor(128*((log(1+255*abs(x/32768))/log(256))))','x');
+mu = inline('sign(x) * floor(128*(log(1+255*abs(x/32768))/log(256)))','x');
 muInv = inline('ceil(32768*sign(y)*(1/255)*(256^abs(y/128)-1))', 'y');
 
 %lin codec algorithm
@@ -19,11 +19,11 @@ linInv = inline('y * 256', 'y');
 srcFile = 'hts1a.wav';
 %srcFile = 'lvb-sym-5-1.wav';
 %srcFile = 'beethoven.wav';
-[wave, fs, nbits] = wavread(srcFile);
+[wave, fs, nbits] = wavread(srcFile,'native'); %native means 16-bit format
 t = 0:1/fs:(length(wave)-1)/fs;
 
-%convert wave to 16-bit audio
-wave = wave*32768;
+%convert wave from 16-bit format to double precision
+wave = double(wave);
 
 %linear encoding
 linEnc = lin(wave);
@@ -51,7 +51,7 @@ for idx = 1:numel(muEnc)
 end
 muDec;
 
-figure('name','Wave Double');
+figure('name','WaveInp Double');
 plot(t,wave);
 figure('name','linEnc Double');
 plot(linEnc);
@@ -64,10 +64,10 @@ plot(t,muDec);
 
 linErr = wave-linDec;
 muErr = wave-transpose(muDec);
-txt = sprintf('Linear encoding avg. error: %f',mean(linErr));
+txt = sprintf('Linear encoding avg. error: %f',mean(abs(linErr)));
 disp(txt)
-txt1 = sprintf('Mu encoding avg.error: %f',mean(muErr));
+txt1 = sprintf('Mu encoding avg.error: %f',mean(abs(muErr)));
 disp(txt1)
 
-%sound(linDec/32768);
+%sound(linDec/32768); %audio data must be double precision
 %sound(muDec/32768);
