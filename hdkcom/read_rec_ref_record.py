06fe2ddd2c8b9be38606a6f038ef03463e235c88
christiantigor <christian.tigor@rocketmail.com>
2015-01-22 12:21:41 +0700

    rearrange hdkcom notes

---
+++ hdkcom/read_rec_ref_record.py
@@ -0,0 +1,9 @@
+#software for open rec_reference.bit
+#to be used for comparison with hdk sniffing
+
+#open encoded file (.bit)
+f = open('rec_ref_record.bit','r')
+encodedFile = f.read()
+h = encodedFile.encode('hex')
+print h
+#print type(encodedFile)
